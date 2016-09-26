79a8f76cf28a06ba742b2925e6f020f5f83d7aab
Arko <dotuplink@gmail.com>
2016-09-25 19:30:27 -0700

    Cleaned up code, untested

--- PID/PID.ino
+++ PID/PID.ino
@@ -1,84 +1,162 @@
+// Simple PID Controller
+// By: Ara Kourchians
+// ----------------------------------------------------------------------------
+
 // LED Variables
 const byte ledPin = 13;
-volatile byte state = LOW;
 
 // Encoder Variables
-const byte encoder_input = 2;
-unsigned long encoder_count = 10000;
+#define TICK_NULL 0
+#define TICK_A 1
+#define TICK_B 2
+#define FORWARD false
+#define BACKWARD true
+const byte encoder_A_pin = 2;
+const byte encoder_B_pin = 3;
+volatile unsigned char last_tick = TICK_NULL;
+long position_ticks = 0;
 unsigned long current_time = 0;
 unsigned long last_time = 0;
+
+// State Variables
+float position = 0.0; // degrees
+float velocity = 0.0; // rpm
+
+// PID Variables
+float target = 0.5; 
+float P_gain = 1;
+float I_gain = 0.01;
+float D_gain = 0;
+float error = 0;
+float last_error = 0;
+float sum_of_error = 0;
+float pid_output = 0;
 
 // Motor Variables
 byte AI0_pin = 6;
 byte AI1_pin = 7; 
 byte motor_pin = 5;
-byte dir = 0;
 byte motor_output = 0;
-float velocity = 0;
+volatile boolean direction = FORWARD;
 
-// PID Variables
-float target = 0.5; 
-float P_gain = 100;
-float I_gain = 500;
-float D_gain = 0;
-float error = target - encoder_count;
-float last_error = 0;
-float sum_of_error = 0;
-float pid_output = 0;
-
-void setup() {
+// Runs first and once
+void setup() 
+{
   pinMode(ledPin, OUTPUT);
-  pinMode(encoder_input, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(encoder_input), encoderTick, CHANGE);
+  
+  // Setup encoder interrupt routines
+  pinMode(encoder_A_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoder_A_pin), encoderTickA, CHANGE);
+
+  pinMode(encoder_B_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoder_B_pin), encoderTickB, CHANGE);
 
   // Set Motor Direction
-  digitalWrite(AI0_pin, dir);
-  digitalWrite(AI1_pin, !dir);
+  setDirection(FORWARD);
+
   // Set Motor Speed to Zero
   analogWrite(motor_pin, motor_output);
   
+  // Initialize Serial Bus
   Serial.begin(115200);
 }
 
-void loop() {
-  digitalWrite(ledPin, state);
-  
-  Serial.print(target*100);
-  Serial.print(" ");
-  Serial.println(velocity*100);
+// Main Loop
+void loop() 
+{
+  digitalWrite(ledPin, direction);
 
-  // Update
-  velocity = 420.0/encoder_count;
-  
-  last_error = error;
-  error = target - velocity;
-  sum_of_error = sum_of_error + error;
-  if(sum_of_error > 1023)
+  // Update Position and Velocity
+  position = ((float)position_ticks)*(360/1800.0);
+  velocity = (0.000555)/(0.000001*((float)(current_time - last_time + 1)));
+
+  // PID Calculations
+  last_error = error;           // Store the last error for derivative
+  error = target - velocity;    // Calculate new velocity error
+  sum_of_error = sum_of_error + error;    // Accumulate error
+  if(sum_of_error > 1023)                 // Saturate to prevent large I term
   {
     sum_of_error = 1023;
   }
   
   // PID Control
-  pid_output = P_gain*error + I_gain*sum_of_error;
-  if(pid_output/4 > 254)
+  pid_output = P_gain*error + I_gain*sum_of_error;  // PID
+
+  if(pid_output > 1.0)                              // Hard limit PID output
   {
-    motor_output = 255;
+    pid_output = 1.0;
   }
-  else
+  else if(pid_output < -1.0)
   {
-    motor_output = pid_output/4;
+    pid_output = -1.0;
   }
-  analogWrite(motor_pin, motor_output);
+
+  if(pid_output >= 0)                               // Set direction
+  {
+    setDirection(FORWARD);
+  }
+  else if(pid_output < 0)
+  {
+    setDirection(BACKWARD);
+  }
+
+  motor_output = (unsigned char)(255.0*abs(pid_output)); // Calculate
+
+  analogWrite(motor_pin, motor_output);             // Apply output to motor 
   
-  // Reset Encoder Count (to achieve velocity)
-  //encoder_count = 0;
+  // Print out data
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.println(velocity);
 
   delay(50);
 }
 
-void encoderTick() {
-  state = !state;
-  current_time = micros();
-  encoder_count = current_time - last_time;
+void setDirection(boolean input_direction)
+{
+  // Set Motor Direction
+  digitalWrite(AI0_pin, input_direction);
+  digitalWrite(AI1_pin, !input_direction);
+}
+
+void encoderTickA() 
+{
   last_time = current_time;
+  current_time = micros();
+  
+  if(last_tick == TICK_A) 
+  {
+    direction = !direction;
+  }
+  last_tick = TICK_A;
+
+  if(direction == FORWARD) 
+  {
+    position_ticks++;    
+  }
+  else if(direction == BACKWARD) 
+  {
+    position_ticks--;
+  }
+}
+
+void encoderTickB() 
+{
+  last_time = current_time;
+  current_time = micros();
+
+  if(last_tick == TICK_B)
+  {
+    direction = !direction;
+  }
+  last_tick = TICK_B;
+
+  if(direction == FORWARD) 
+  {
+    position_ticks++;    
+  }
+  else if(direction == BACKWARD) 
+  {
+    position_ticks--;
+  }
 }
